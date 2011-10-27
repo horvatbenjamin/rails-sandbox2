be12e26abbc784e0a8d0440ea2cc19f1b8040cd5
Benjamin Horvat <horvatbenjamin@gmail.com>
2011-10-27 23:33:49 +0200

    tests are fucked up

--- Gemfile
+++ Gemfile
@@ -43,4 +43,5 @@ group :test do
   gem 'turn', :require => false
   gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
+  gem 'spork', '0.9.0.rc8'
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
 class PagesController < ApplicationController
+  
   def home
+  @global_title = "Ruby on Rails Tutorial Sample App"
+  @local_title = "Home"
+  @title = @global_title + " | " +@local_title
   end
 
   def contact
+  @global_title = "Ruby on Rails Tutorial Sample App"
+  @local_title = "Contact"
+  @title = @global_title + " | " +@local_title
   end
 
+  def about
+  @global_title = "Ruby on Rails Tutorial Sample App"
+  @local_title = "About"
+  @title = @global_title + " | " +@local_title
+  end
 end
